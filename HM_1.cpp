dba79ec1286ce330b9ead81103107ad261807c5a
Voloshka05 <volooleksandra@gmail.com>
2024-12-29 23:59:44 +0200

    Update file

--- HM_1.cpp
+++ HM_1.cpp
@@ -4,28 +4,11 @@
 
 using namespace std;
 
-void task1() {
-    for (int i = 0; i < 5; i++) {
-        cout << "Task 1 iteration " << i + 1 << endl;
-        this_thread::sleep_for(chrono::seconds(1));
-    }
-}
-
-void task2() {
-    for (int i = 0; i < 5; i++) {
-        cout << "Task 2 iteration " << i + 1 << endl;
-        this_thread::sleep_for(chrono::seconds(1));
-    }
-}
-
 int main() {
     thread thread1(task1);
     thread thread2(task2);
 
-    thread1.join();
-    thread2.join();
-
-    cout << "Both streams have been completed" << endl;
+    cout << "!Hello world!" << endl;
 
     return 0;
 }
